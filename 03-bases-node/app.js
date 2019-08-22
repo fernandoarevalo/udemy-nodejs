ca9ef9e5ef072d746f38ef48f3d0621f93dc6b7c
Fernando Arévalo <fernando.arevalo9311@gmail.com>
2019-08-21 22:41:14 -0500

    03 bases node

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,7 +1,9 @@
 
 const { crearArchivo } = require('./multiplicar/multiplicar')
 
-let base = '8';
+let argv = process.argv
+let parametro = argv[2]
+let base = parametro.split(`=`)[1]
 
 crearArchivo(base)
     .then(archivo => console.log(`Archivo creado: ${archivo}`))
